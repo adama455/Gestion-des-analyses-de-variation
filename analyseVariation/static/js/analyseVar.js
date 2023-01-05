26404508ad008db6e9577a390f341e92949e5d7b
Adama@455 <1994adamandiaye@gmail.com>
2023-01-05 09:51:55 +0000

    modif js

--- analyseVariation/static/js/analyseVar.js
+++ analyseVariation/static/js/analyseVar.js
@@ -30,20 +30,36 @@ pourquoi.addEventListener('click', () => {
 pourquoiSuivant.addEventListener('click', ()=> {
     let i = 1;
     alert('Please enter')
-    // divPourquoi.innerHTML += `
-    //     <div class='row pourquoi mb-2 d-flex align-items-center'>
-    //         <label for='pourquoi1' class='col-3'>Pourquoi1</label>
-        
-    //         <div class="col-8" id="pourquoi2">
-    //             <input type="text" name="pourquoi1" id="pourquoi2" class="col-12">
-    //         </div>
-    //         <div class="icon-add col-1 d-flex justify-content-end">
-    //             <i class="fa-solid fa-circle-plus fs-4" style="cursor: pointer;" id="pourquoi2"></i>
-    //         </div>
-    //     </div> 
 
-    // `
-    i++;
+    const div = document.createElement('div')
+    div.className = 'row pourquoi mb-2 d-flex align-items-center'
+    const label = document.createElement('label')
+    label.className = 'col-3'
+    const divInput = document.createElement('div')
+    divInput.className = 'col-8'
+    const input = document.createElement('input')
+    input.className = 'col-12'
+    const divIcon = document.createElement('div')
+    divIcon.className = 'icon-add col-1 d-flex justify-content-end'
+    const icon = document.createElement('i')
+    icon.className='fa-solid fa-circle-plus fs-4'
+    icon.style
+
+    divPourquoi.innerHTML += `
+
+    <div class='row pourquoi mb-2 d-flex align-items-center'>
+        <label for='pourquoi1' class='col-3'>Pourquoi1</label>
+    
+        <div class="col-8" id="pourquoi">
+            <input type="text" name="pourquoi1" id="pourquoi2" class="col-12">
+        </div>
+        <div class="icon-add col-1 d-flex justify-content-end">
+            <i class="fa-solid fa-circle-plus fs-4" style="cursor: pointer;" id="pourquoi">
+        </div>
+    </div> 
+    
+    `
+
 
     
 });
